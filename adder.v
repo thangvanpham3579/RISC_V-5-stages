32c61e8d7959ac1be034d2b9948046cab7cbf597
thangvanpham3579 <thang782002@gmail.com>
2025-04-18 02:56:45 +0700

    Xóa tất cả các file code

--- adder.v
+++
@@ -1,6 +0,0 @@
-`timescale 1ns / 1ps
-module adder(s,t,u);
-input [31:0] s,t;
-output [31:0] u;
-assign u = s+t;
-endmodule
--- mux.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-module mux(m,n,sel,out);
-input [31:0]m,n;
-input sel;
-output [31:0]out;
-assign out = sel ? n : m;
-endmodule 
